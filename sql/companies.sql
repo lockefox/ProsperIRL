54e6ce65b7771cb0731df847a4098b889327954d
John Purcell <locke.renard@gmail.com>
2014-08-31 11:22:50 -0600

    adding NASDAQ company listing and company profile sql

---
+++ sql/companies.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+	`symbol`	VARCHAR(6) NOT NULL,
+	`name` 		VARCHAR(128) NOT NULL,
+	`IPOyear`	int(4) NULL,
+	`sector`	VARCHAR(32)	NOT NULL,
+	`industry`	VARCHAR(32)	NOT NULL,
+	`summary`	VARCHAR(128) NULL,
+	PRIMARY KEY (symbol))
+ENGINE=InnoDB DEFAULT CHARSET=latin1
