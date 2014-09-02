58e4ca7b8f841b167713385d533469089755d9b6
John Purcell <locke.renard@gmail.com>
2014-09-02 10:21:01 -0600

    Added `exchange` col to table

--- sql/companies.sql
+++ sql/companies.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `companies`;
 CREATE TABLE `companies` (
 	`symbol`	VARCHAR(6) NOT NULL,
+	`exchange`	VARCHAR(6) NOT NULL,
 	`name` 		VARCHAR(128) NOT NULL,
 	`IPOyear`	int(4) NULL,
 	`sector`	VARCHAR(32)	NOT NULL,
