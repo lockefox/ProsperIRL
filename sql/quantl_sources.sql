e70921044d0721d4244812a19d6c215036bd7474
John Purcell <jpurcell@micron.com>
2014-09-04 16:58:59 -0600

    validated quantl_sources.sql

--- sql/quantl_sources.sql
+++ sql/quantl_sources.sql
@@ -3,11 +3,11 @@ CREATE TABLE `quantl_sources` (
 	`source`	TINYTEXT NOT NULL,
 	`code`		TINYTEXT NOT NULL,
 	`name`		TINYTEXT NULL,
-	`frequency`	ENUM('none','daily','weekly','monthly','quarterly','annual') 'none',
+	`frequency`	ENUM('none','daily','weekly','monthly','quarterly','annual') NULL DEFAULT 'none' ,
 	`startdate` DATE NULL,
 	`updated_at` DATETIME NOT NULL,
 	`id`		INT(8) NULL,
 	`columns`	MEDIUMTEXT NULL,
-	PRIMARY KEY (source,code))
+	PRIMARY KEY (source(255),code(255)))
 	ENGINE=InnoDB DEFAULT CHARSET=latin1;
 CREATE INDEX source_freshness ON quantl_sources(updated_at)
