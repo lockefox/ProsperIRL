d7a74c59f6b01c6e5061b3fb35fcb9aa3271c246
John Purcell <locke.renard@gmail.com>
2014-09-21 11:15:10 -0600

    adding setup.py as enviroment initializer:

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+from os import path
+import urllib2
+import ConfigParser
+import pypyodbc
+from datetime import datetime
+
+conf = ConfigParser.ConfigParser()
+conf.read(['init.ini','init_local.ini'])
