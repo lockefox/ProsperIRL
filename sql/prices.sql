f2eec8795f9d60327fea644759dee70f79bc7b2b
John Purcell <jpurcell@micron.com>
2014-09-24 15:25:02 -0600

    updated prices.sql to match inputs

--- sql/prices.sql
+++ sql/prices.sql
@@ -1,12 +1,18 @@
 DROP TABLE IF EXISTS `prices`;
 CREATE TABLE `prices` (
 	`quote_date`	DATE NOT NULL,
-	`symbol`		VARCHAR(6) NOT NULL,
-	`open_price`	FLOAT(5,2) NULL,
-	`high_price`	FLOAT(5,2) NULL,
-	`low_price`		FLOAT(5,2) NULL,
-	`close_price`	FLOAT(5,2) NOT NULL,
-	`volume`		BIGINT(12) NULL,
-	`adj_close`		FLOAT(5,2) NULL,
+	`symbol`		VARCHAR(8) NOT NULL,
+	`open_price`	FLOAT(6,2) NOT NULL,
+	`high_price`	FLOAT(6,2) NOT NULL,
+	`low_price`		FLOAT(6,2) NOT NULL,
+	`close_price`	FLOAT(6,2) NOT NULL,
+	`volume`		BIGINT(12) NOT NULL,
+	`ex_dividend`	FLOAT(6,4) NULL,
+	`split_ratio`	FLOAT(6,4) NULL,
+	`adj_open`		FLOAT(6,2) NULL,
+	`adj_close`		FLOAT(6,2) NULL,
+	`adj_high`		FLOAT(6,2) NULL,
+	`adj_low`		FLOAT(6,2) NULL,
+	`adj_volume`	BIGINT(12) NOT NULL,
 	PRIMARY KEY (quote_date,symbol))
 ENGINE=InnoDB DEFAULT CHARSET=latin1
